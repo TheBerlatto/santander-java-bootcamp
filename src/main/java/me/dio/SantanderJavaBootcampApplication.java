c587ce2f5eabf47b620b5da9555745d0874dca01
Eduardo Berlatto <eduardoberlatto@gmail.com>
2024-07-04 15:48:44 -0300

    Initial commit

---
+++ src/main/java/me/dio/SantanderJavaBootcampApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderJavaBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderJavaBootcampApplication.class, args);
+	}
+
+}
